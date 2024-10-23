f500777a752a2511c90568ef3cbc8d859102e628
Cata_a <108532800+Cata-a1138@users.noreply.github.com>
2024-10-23 16:47:26 +0800

    refactor: 重构 item 加载

--- data/items.lua
+++ data/items.lua
@@ -1,16 +1,13 @@
-local filesToLoad = {
-	-- 需要按照以下格式添加,否则不会读取文件
-	'items/normal',
-	'items/food',
+local ITEMS = {}
+local item_files = {
+	-- 必须将不含扩展名的文件名填写在下面, 否则不会读取文件
+	-- 示例: data\items\food.lua, 填写: food
+	'food',
+	'normal',
 }
 
-local fileContents = {}
-
-for _, fileName in ipairs(filesToLoad) do
-	local content = data(fileName)
-	for k, v in pairs(content) do
-		fileContents[k] = v
-	end
+for _, v in pairs(item_files) do
+	lib.table.merge(ITEMS, lib.load(('data.items.%s'):format(v)))
 end
 
-return fileContents
+return ITEMS
